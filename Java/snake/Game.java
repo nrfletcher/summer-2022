62706abd9a50929508e3888528293b5ec6bebb90
Nesta Riley Fletcher <89424122+nrfletcher@users.noreply.github.com>
2022-07-16 22:49:28 -0400

    Create Game.java

---
+++ Java/snake/Game.java
@@ -0,0 +1,12 @@
+public class Game {
+
+    public static void main(String[] args) {
+        startGame();
+    }
+
+    public static void startGame() {
+
+        Frame frame = new Frame();
+    }
+
+}
